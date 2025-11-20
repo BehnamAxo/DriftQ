d10496c21fe07dca512716d281286e925b1889ad
Behnam Amiri <behamiridev@gmail.com>
2025-11-19 17:48:31 -0700

    Implement in-memory topic create/list on broker

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,8 +1,12 @@
 package broker
 
-import "context"
+import (
+	"context"
+	"errors"
+	"sort"
+	"sync"
+)
 
-// The simplest MVP message
 type Message struct {
 	Key   []byte
 	Value []byte
@@ -19,11 +23,48 @@ type Broker interface {
 
 // InMemoryBroker is our first implementation. For sure later we'll replace pieces with WAL, scheduler, partitions, etc
 type InMemoryBroker struct {
+	mu     sync.RWMutex
 	topics map[string][]Message
 }
 
+// NewInMemoryBroker creates a new in-memory broker instance.
 func NewInMemoryBroker() *InMemoryBroker {
 	return &InMemoryBroker{
 		topics: make(map[string][]Message),
 	}
 }
+
+// CreateTopic creates a topic if it does not already exist. And partitions is ignored for now (TODO: real partitioning later).
+func (b *InMemoryBroker) CreateTopic(_ context.Context, name string, partitions int) error {
+	if name == "" {
+		return errors.New("topic name cannot be empty")
+	}
+	if partitions <= 0 {
+		return errors.New("partitions must be > 0")
+	}
+
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if _, exists := b.topics[name]; exists {
+		return nil
+	}
+
+	b.topics[name] = nil // no messages yet
+	return nil
+}
+
+// ListTopics returns the list of topic names (sorted for stability).
+func (b *InMemoryBroker) ListTopics(_ context.Context) ([]string, error) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	names := make([]string, 0, len(b.topics))
+	for name := range b.topics {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	return names, nil
+}
+
+// TODO: Produce + Consume will be implemented in later steps.
